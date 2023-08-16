06d2cd0ccaf6f7c5d15e60782cb3faa08cafaaeb
vansh1504.be21@chitkara.edu.in <vansh1504.be21@chitkara.edu.in>
2023-08-16 19:56:44 +0530

    Improve the login part

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var hamburger = document.getElementById('hamburger');
 var res_nav = document.getElementById('res-nav');
 var back = document.getElementById("back");
-var login = document.getElementsByClassName("login");
+var login = document.getElementsByClassName("loginContainer");
 var body = document.getElementsByTagName("body");
 var loginicon = document.getElementById("loginicon");
 var cancel = document.getElementById("imagecross");
 
 function showlogin()
 {
-    login[0].style.visibility="visible";
+    // login[0].style.visibility="visible";
+    login[0].style.transform="scaleY(1)";
     // body[0].style.opacity="0.5";
     
 }
 function hidelogin()
 {
-    login[0].style.visibility="hidden";
+    // login[0].style.visibility="hidden";
+    login[0].style.transform="scaleY(0)";
     // body[0].style.opacity="1.0";
     
 }
