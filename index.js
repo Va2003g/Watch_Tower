8397692271cbb2c9d9a9dbebd348663a650cd7e4
vansh1504.be21@chitkara.edu.in <vansh1504.be21@chitkara.edu.in>
2023-10-22 00:48:04 +0530

    Adding JS to data

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// var hamburger = document.getElementById('hamburger');
-var res_nav = document.getElementById("res-nav");
-var back = document.getElementById("back");
 var login = document.getElementsByClassName("loginContainer");
 var body = document.getElementsByTagName("body");
 var loginicon = document.getElementById("loginicon");
@@ -13,17 +10,7 @@ function showlogin() {
 function hidelogin() {
   login[0].style.transform = "scaleY(0)";
 }
-// console.log(back,res_nav);
-function showResNav() {
-  res_nav.style.visibility = "visible";
-}
 
-function hideResNav() {
-  res_nav.style.visibility = "hidden";
-}
-
-// hamburger.addEventListener('click',showResNav);
-// back.addEventListener('click',hideResNav);
 loginicon.addEventListener("click", showlogin);
 cancel.addEventListener("click", hidelogin);
 
@@ -50,6 +37,7 @@ function menWatches() {
 
 var userName;
 function userLogin(event) {
+  console.log(event);
   userName = event.target[0].value;
   event.preventDefault();
   updateName(userName);
@@ -71,4 +59,202 @@ function updateName(name)
 {
   loginicon.innerText = name;
   sessionStorage.setItem("UserName", name);
+}
+
+const menData = [
+  {
+    image:"m1",
+    title:"fasttrack"
+  },
+  {
+    image:"m2",
+    title:"TITAN"
+  },
+  {
+    image:"m3",
+    title:"FOSSIL"
+  },
+  {
+    image:"m4",
+    title:"TOMMY HILFIGER"
+  },
+  {
+    image:"m5",
+    title:"ARMANI EXCHANGE"
+  },
+  {
+    image:"m6",
+    title:"DANIEL WELLINGTON"
+  },
+  {
+    image:"m7",
+    title:"KENNETH COLE"
+  },
+  {
+    image:"m8",
+    title:"TITAN"
+  },
+]
+
+var menSection = document.getElementById('imagesM');
+
+for(var i = 0;i<menData.length;i++)
+{
+  let temp = `<div class="product-box">
+  <img src="./images/${menData[i].image}.jpg" alt="" class="product-img">
+  <h2 class="product-title">${menData[i].title}</h2>
+  <span class="product-price">Rs:- ${Math.round(Math.random()*10000+100)}</span>
+  <i class="fas fa-shopping-cart add-cart"></i>
+</div>`;
+  menSection.innerHTML+=temp;
+}
+
+const womenData = [
+  {
+    image:"w1",
+    title:"TITAN"
+  },
+  {
+    image:"w2",
+    title:"RADO"
+  },
+  {
+    image:"w9",
+    title:"SONATA"
+  },
+  {
+    image:"w4",
+    title:"GUCCI"
+  },
+  {
+    image:"w5",
+    title:"TIMEX"
+  },
+  {
+    image:"w6",
+    title:"FASTRACK"
+  },
+  {
+    image:"w7",
+    title:"BALMAIN"
+  },
+  {
+    image:"w8",
+    title:"SEIKO"
+  },
+]
+
+var womenSection = document.getElementById('imagesw');
+
+for(var i = 0;i<womenData.length;i++)
+{
+  let temp = `<div class="product-box">
+  <img src="./images/${womenData[i].image}.jpg" alt="" class="product-img">
+  <h2 class="product-title">${womenData[i].title}</h2>
+  <span class="product-price">Rs:- ${Math.round(Math.random()*10000+100)}</span>
+  <i class="fas fa-shopping-cart add-cart"></i>
+</div>`;
+  womenSection.innerHTML+=temp;
+}
+
+const kidsData = [
+  {
+    image:"k1",
+    title:"G-SHOCK"
+  },
+  {
+    image:"k2",
+    title:"SONATA KIDS"
+  },
+  {
+    image:"k3",
+    title:"TITAN"
+  },
+  {
+    image:"k4",
+    title:"SONICX"
+  },
+  {
+    image:"k5",
+    title:"CALVIN KLEIN"
+  },
+  {
+    image:"k6",
+    title:"LACOSTE"
+  },
+  {
+    image:"k7",
+    title:"DIGITAL APPLE"
+  },
+  {
+    image:"k8",
+    title:"FOSSIL"
+  },
+]
+
+var kidsSection = document.getElementById('imagesk');
+
+let extension = "jpeg";
+for(var i = 0;i<womenData.length;i++)
+{
+  if(i==6||i==7)
+  {
+    extension = "jpg";
+  }
+  let temp = `<div class="product-box">
+  <img src="./images/${kidsData[i].image}.${extension}" alt="" class="product-img">
+  <h2 class="product-title">${kidsData[i].title}</h2>
+  <span class="product-price">Rs:- ${Math.round(Math.random()*10000+100)}</span>
+  <i class="fas fa-shopping-cart add-cart"></i>
+</div>`;
+  kidsSection.innerHTML+=temp;
+}
+
+const wallData = [
+  {
+    image:"c1",
+    title:"CLARKE"
+  },
+  {
+    image:"c2",
+    title:"MINERVA"
+  },
+  {
+    image:"c3",
+    title:"AUSTERE"
+  },
+  {
+    image:"c4",
+    title:"CASABLANCA"
+  },
+  {
+    image:"c5",
+    title:"CORSICA"
+  },
+  {
+    image:"c6",
+    title:"SENSU"
+  },
+  {
+    image:"c7",
+    title:"QUARTZ"
+  },
+  {
+    image:"c8",
+    title:" MONT BLANC"
+  },
+]
+
+var wallSection = document.getElementById('imagesc');
+
+extension = "jpeg";
+for(var i = 0;i<womenData.length;i++)
+{
+  let temp = `<div class="product-box">
+  <img src="./images/${wallData[i].image}.${extension}" alt="" class="product-img">
+  <h2 class="product-title">${wallData[i].title}</h2>
+  <span class="product-price">Rs:- ${Math.round(Math.random()*10000+100)}</span>
+  <i class="fas fa-shopping-cart add-cart"></i>
+</div>`;
+  wallSection.innerHTML+=temp;
 }
